98027ae50ffa93675901859187836cd48851851a
diego veloza <diego.veloza@unipamplona.edu.co>
2024-08-13 10:54:31 -0500

    paginas de los roles docentes y estudiantes

---
+++ src/app/dashboard/docente/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocentesPage = () => {
+    return (
+        <div>
+            <h1>DocentesPage</h1>
+        </div>
+    );
+};
+
+export default DocentesPage;
---
+++ src/app/dashboard/estudiante/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EstudiantePage = () => {
+    return (
+        <div>
+            <h1>EstudiantePage</h1>
+        </div>
+    );
+};
+
+export default EstudiantePage;
