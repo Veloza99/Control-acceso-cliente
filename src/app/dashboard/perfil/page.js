f3a0ef752b69c369b6c6e7be1e028cfbf63ad1d7
diego veloza <diego.veloza@unipamplona.edu.co>
2024-09-02 12:44:50 -0500

    se modifico para traer el perfil

--- src/app/dashboard/perfil/page.js
+++ src/app/dashboard/perfil/page.js
@@ -4,7 +4,25 @@ import React from 'react';
 import { useGeneral } from '@/context/GeneralContext';
 
 const PerfilPage = () => {
-    const { user } = useGeneral();
+    const { user, getImagenPerfil } = useGeneral();
+
+    // Función para obtener la URL de la imagen de perfil
+    const getProfileImage = async () => {
+        if (user && user.picProfile) {
+            return await getImagenPerfil(user.picProfile);
+        }
+        return null;
+    };
+
+    // Obtener la imagen de perfil
+    const [profileImage, setProfileImage] = React.useState(null);
+    React.useEffect(() => {
+        const fetchProfileImage = async () => {
+            const image = await getProfileImage();
+            setProfileImage(image);
+        };
+        fetchProfileImage();
+    }, [user, getImagenPerfil]);
 
     if (!user) {
         return <p>Cargando...</p>;
@@ -14,22 +32,34 @@ const PerfilPage = () => {
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">Perfil del Usuario</h1>
             <div className="bg-white shadow rounded-lg p-6">
-                <div className="grid grid-cols-1 gap-6">
-                    <div>
-                        <label className="block text-xl font-medium text-blue-800">Nombre</label>
-                        <p className="mt-1 text-lg text-gray-700">{user.firstName} {user.lastName}</p>
+                <div className="flex items-center mb-6">
+                    <div className="flex-shrink-0 mr-6">
+                        <p className="block text-xl font-medium text-blue-800">Foto</p>
+                        {profileImage ? (
+                            <img src={profileImage} alt="Perfil" className="w-38 h-38 rounded-full" />
+                        ) : (
+                            <div className="w-38 h-38 flex items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-gray-500">
+                                <p>No tiene foto</p>
+                            </div>
+                        )}
                     </div>
-                    <div>
-                        <label className="block text-xl font-medium text-blue-800">Identificacion</label>
-                        <p className="mt-1 text-lg text-gray-700">{user.identificacion}</p>
-                    </div>
-                    <div>
-                        <label className="block text-xl font-medium text-blue-800">Correo Electrónico</label>
-                        <p className="mt-1 text-lg text-gray-700">{user.email}</p>
-                    </div>
-                    <div>
-                        <label className="block text-xl font-medium text-blue-800">Rol</label>
-                        <p className="mt-1 text-lg text-gray-700">{user.role.toUpperCase()}</p>
+                    <div className="flex flex-col">
+                        <div>
+                            <label className="block text-xl font-medium text-blue-800">Nombre</label>
+                            <p className="mt-1 text-lg text-gray-700">{user.firstName} {user.lastName}</p>
+                        </div>
+                        <div>
+                            <label className="block text-xl font-medium text-blue-800">Identificación</label>
+                            <p className="mt-1 text-lg text-gray-700">{user.identificacion}</p>
+                        </div>
+                        <div>
+                            <label className="block text-xl font-medium text-blue-800">Correo Electrónico</label>
+                            <p className="mt-1 text-lg text-gray-700">{user.email}</p>
+                        </div>
+                        <div>
+                            <label className="block text-xl font-medium text-blue-800">Rol</label>
+                            <p className="mt-1 text-lg text-gray-700">{user.role.toUpperCase()}</p>
+                        </div>
                     </div>
                 </div>
             </div>
